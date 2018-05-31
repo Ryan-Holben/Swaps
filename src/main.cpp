d45ad5cbd5e0a01189d5597602da95af176d4310
Ryan Holben <rgholben@gmail.com>
2018-05-31 13:15:24 -0700

    Implemented 2x2 square

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,20 @@ int main() {
   std::vector<swapPtr> swaps;
 
   // Fill those containers with a 1D array to do permutations on
-  create1DArrangement(5, &start, swaps);
+  create1DArrangement(4, &start, swaps);
+  // start = Arrangement(4);
+  // start.defineAdjacency(0, 1);
+  // std::string name = "a";
+  // swaps.emplace_back(std::make_shared<Swap>(0, 1, name));
+  // start.defineAdjacency(0, 2);
+  // name = "b";
+  // swaps.emplace_back(std::make_shared<Swap>(0, 2, name));
+  // start.defineAdjacency(1, 3);
+  // name = "c";
+  // swaps.emplace_back(std::make_shared<Swap>(1, 3, name));
+  // start.defineAdjacency(2, 3);
+  // name = "d";
+  // swaps.emplace_back(std::make_shared<Swap>(2, 3, name));
 
   // Initialize & build a permutahedron
   Permutahedron perm = Permutahedron();
