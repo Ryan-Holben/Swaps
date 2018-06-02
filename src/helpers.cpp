b6171a5b098b49004aa085301fb8f36e4a2dcda3
Ryan Holben <rgholben@gmail.com>
2018-06-02 15:11:16 -0700

    2D helper function complete

--- src/helpers.cpp
+++ src/helpers.cpp
@@ -18,23 +18,59 @@ bool create1DArrangement(size_t length, Arrangement* arr, std::vector<swapPtr>& 
   return true;
 }
 
-// 0 ... width-1
-// width ... 2*width-1
-// 
+// Convert 2D coordinates to 1D coordinates, assuming we start at (0,0) in 2D and 0 in 1D
+inline size_t convert2Dto1D(size_t x, size_t y, size_t W, size_t H) {
+  return y * H + x;
+}
 
-bool create2DArrangement(size_t length, size_t width, Arrangement* arr, std::vector<swapPtr>& swaps) {
+bool create2DArrangement(size_t width, size_t height, Arrangement* arr, std::vector<swapPtr>& swaps) {
   if (arr == nullptr) {
     return false;
   }
+  const size_t totalLength = width * height;
+  *arr = Arrangement(totalLength); // This is surely inefficient
 
-  *arr = Arrangement(length*width); // This is surely inefficient
   std::string strName;
   char name = 'a';
-  for (size_t i = 0; i < length-1; ++i) {
-    arr->defineAdjacency(i, i+1);
+  size_t thisSpace, adjacentSpace;
+
+  // Block from (0, 0) through (width - 2, height - 2)
+  for (size_t y = 0; y < height-1; ++y) {
+    for (size_t x = 0; x < width-1; ++x) {
+      thisSpace = convert2Dto1D(x, y, width, height);
+      adjacentSpace = thisSpace + 1;
+      arr->defineAdjacency(thisSpace, adjacentSpace);
+      strName = name;
+      swaps.emplace_back(std::make_shared<Swap>(thisSpace, adjacentSpace, strName));
+      name += 1;
+
+      adjacentSpace = thisSpace + width;
+      arr->defineAdjacency(thisSpace, adjacentSpace);
+      strName = name;
+      swaps.emplace_back(std::make_shared<Swap>(thisSpace, adjacentSpace, strName));
+      name += 1;
+    }
+  }
+
+  // width - 1 column
+  for (size_t y = 0; y < height-1; ++y) {
+    thisSpace = convert2Dto1D(width-1, y, width, height);
+    adjacentSpace = thisSpace + width;
+    arr->defineAdjacency(thisSpace, adjacentSpace);
     strName = name;
-    swaps.emplace_back(std::make_shared<Swap>(i, i+1, strName));
+    swaps.emplace_back(std::make_shared<Swap>(thisSpace, adjacentSpace, strName));
     name += 1;
   }
+
+  // height - 1 row
+  for (size_t x = 0; x < width-1; ++x) {
+    thisSpace = convert2Dto1D(x, height-1, width, height);
+    adjacentSpace = thisSpace + 1;
+    arr->defineAdjacency(thisSpace, adjacentSpace);
+    strName = name;
+    swaps.emplace_back(std::make_shared<Swap>(thisSpace, adjacentSpace, strName));
+    name += 1;
+  }
+
   return true;
 }
